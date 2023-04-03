e54fbd29141deaba622fdff11bd109ea7bce4e1e
lucian-lgtm <63681775+lucian-lgtm@users.noreply.github.com>
2023-04-03 16:19:49 -0300

    Road Spawner

---
+++ Assets/Scripts/WayScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayScript : MonoBehaviour
+{
+    public GameObject Road;
+    public Transform LocationSpawn;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Instantiate(Road, LocationSpawn.position, Quaternion.identity);
+    }
+}

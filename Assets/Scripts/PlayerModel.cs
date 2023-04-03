52c5599fae27c7e4f6d7902824f682821cd974b6
lucian-lgtm <63681775+lucian-lgtm@users.noreply.github.com>
2023-04-03 16:32:56 -0300

    Road Spawner

--- Assets/Scripts/PlayerModel.cs
+++ Assets/Scripts/PlayerModel.cs
@@ -7,4 +7,18 @@ public class PlayerModel : MonoBehaviour
     public float _speedX = 100f;
     public float _speedY = 100f;
 
+
+
+
+
+
+
+
+
+
+
+
+    /*
+     Patrones: (Main)MVC. (Obstacles)Factory, Pool, ????. (Conecciones)EventManager.  
+     */
 }

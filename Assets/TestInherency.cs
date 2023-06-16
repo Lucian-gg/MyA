c065b5d9a1ac1e2b4b269b6b1bd9fa0e0f3ed100
lucian-lgtm <63681775+lucian-lgtm@users.noreply.github.com>
2023-06-16 12:28:58 -0300

    ?

---
+++ Assets/TestInherency.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInherency : MonoBehaviour
+{
+
+
+    public void PrintName(string esta)
+    {
+        print(esta);
+    }
+}
+
